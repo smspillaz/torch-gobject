9076c7ce648771c3143d7e1962b6da3d4fe00d04
Sam Spilsbury <smspillaz@gmail.com>
2020-12-31 19:33:58 -0500

    tensor: Don't set tensor:data on construction
    The default is nullptr and that happens because we zero-initialize anyway.

--- torch-gobject/torch-tensor.cpp
+++ torch-gobject/torch-tensor.cpp
@@ -778,8 +778,7 @@ torch_tensor_class_init (TorchTensorClass *klass)
                                                         "Data of the Tensor",
                                                         G_VARIANT_TYPE ("av"),
                                                         nullptr,
-                                                        static_cast <GParamFlags> (G_PARAM_READWRITE |
-                                                                                   G_PARAM_CONSTRUCT));
+                                                        static_cast <GParamFlags> (G_PARAM_READWRITE));
 
   /**
    * TorchTensor:dtype:
