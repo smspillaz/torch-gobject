c07ab22059396ab0fff09cecd1976bb97a155811
Sam Spilsbury <smspillaz@gmail.com>
2021-01-01 17:34:56 -0500

    torch-gobject: Use consistent project name in license headers

--- torch-gobject/torch-tensor.cpp
+++ torch-gobject/torch-tensor.cpp
@@ -5,12 +5,12 @@
  *
  * Copyright (C) 2020 Sam Spilsbury.
  *
- * libanimation is free software: you can redistribute it and/or
+ * torch-gobject is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
  *
- * libanimation is distributed in the hope that it will be useful,
+ * torch-gobject is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
