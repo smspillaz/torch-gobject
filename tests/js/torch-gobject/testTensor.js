d51524c3f08a4dd63bdfd534737faf290e742c55
Sam Spilsbury <smspillaz@gmail.com>
2021-01-03 20:36:48 -0500

    tests: Add tests for mutating data with index_list_put_inplace

--- tests/js/torch-gobject/testTensor.js
+++ tests/js/torch-gobject/testTensor.js
@@ -110,6 +110,49 @@ describe('TorchTensor', function() {
     expect(tensor_indexed.get_tensor_data().deep_unpack()).toEqual([7.0, 8.0]);
   });
 
+  // Broken: get_tensor_data can't handle single value
+  xit('can be indexed by ints to get a single vlaue', function() {
+    let opts = new Torch.TensorOptions({ dtype: GObject.TYPE_DOUBLE });
+    let tensor = Torch.linspace_double(1.0, 8.0, 8, opts);
+    let tensor_reshaped = tensor.reshape([2, 2, 2]);
+    let indices = [Torch.Index.new_int (1), Torch.Index.new_int (1), Torch.Index.new_int (1)];
+
+    let tensor_indexed = tensor_reshaped.index_list(indices);
+
+    expect(tensor_indexed.get_tensor_data().deep_unpack()).toEqual(8.0);
+  });
+
+  it('can put a single value given by ints index', function() {
+    let opts = new Torch.TensorOptions({ dtype: GObject.TYPE_DOUBLE });
+    let tensor = Torch.linspace_double(1.0, 8.0, 8, opts);
+    let tensor_reshaped = tensor.reshape([2, 2, 2]);
+    let put_indices = [Torch.Index.new_int (1), Torch.Index.new_int (1), Torch.Index.new_int (0)];
+
+    tensor_reshaped.index_list_put_inplace_double (put_indices, 10.0);
+
+    let indices = [Torch.Index.new_int (1), Torch.Index.new_int (1)];
+    let tensor_indexed = tensor_reshaped.index_list(indices);
+
+    expect(tensor_indexed.get_tensor_data().deep_unpack()).toEqual([10.0, 8.0]);
+  });
+
+  it('can put a tensor given by none on last index', function() {
+    let opts = new Torch.TensorOptions({ dtype: GObject.TYPE_DOUBLE });
+    let tensor = Torch.linspace_double(1.0, 8.0, 8, opts);
+    let tensor_reshaped = tensor.reshape([2, 2, 2]);
+    let put_indices = [Torch.Index.new_int (1), Torch.Index.new_int (1), Torch.Index.new_none ()];
+
+    tensor_reshaped.index_list_put_inplace_tensor(
+      put_indices,
+      new Torch.Tensor({ data: new GLib.Variant("v", new GLib.Variant("ad", [2.0, 2.0])) })
+    );
+
+    let indices = [Torch.Index.new_int (1), Torch.Index.new_int (1)];
+    let tensor_indexed = tensor_reshaped.index_list(indices);
+
+    expect(tensor_indexed.get_tensor_data().deep_unpack()).toEqual([2.0, 2.0]);
+  });
+
   it('can be indexed by a slice', function() {
     let opts = new Torch.TensorOptions({ dtype: GObject.TYPE_DOUBLE });
     let tensor = Torch.linspace_double(1.0, 10.0, 10, opts);
