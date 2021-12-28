64de6d699ec76b4bd21d7acdba1348f623a2c11a
Sam Spilsbury <smspillaz@gmail.com>
2021-12-28 13:16:53 +0200

    torch-gobject: Add TorchOptionalValue
    This is used for the sake of language bindings - we want to
    be able to wrap "optional" values (encoded as pointers) using
    a boxed datatype which can be converted later.

---
+++ torch-gobject/torch-optional-value.c
@@ -0,0 +1,88 @@
+/*
+ * torch-gobject/torch-optional-value.c
+ *
+ * Utility boxed type to represent an optional value. A value of %NULL
+ * means that the value is empty.
+ *
+ * Copyright (C) 2022 Sam Spilsbury.
+ *
+ * torch-gobject is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * torch-gobject is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with torch-gobject; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <torch-gobject/torch-optional-value.h>
+
+struct _TorchOptionalValue {
+    GValue internal_value;
+};
+
+/**
+ * torch_optional_value_new_double:
+ * @value: The #double to put in the #TorchOptionalValue.
+ *
+ * Returns: (transfer full): A new #TorchOptionalValue with data @value and type %G_TYPE_DOUBLE
+ */
+TorchOptionalValue *
+torch_optional_value_new_double (double value)
+{
+    TorchOptionalValue *optional_value = g_new0 (TorchOptionalValue, 1);
+    g_value_init (&optional_value->internal_value, G_TYPE_DOUBLE);
+    g_value_set_double (&optional_value->internal_value, value);
+
+    return optional_value;
+}
+
+/**
+ * torch_optional_value_get_double:
+ * @value: The #TorchOptionalValue to get the internal value from.
+ *
+ * It is an error to use this function on any #TorchOptionalValue that does
+ * not contain a #double.
+ *
+ * Returns: A #double with the internal value.
+ */
+double
+torch_optional_value_get_double (TorchOptionalValue *value)
+{
+    return g_value_get_double (&value->internal_value);
+}
+
+/**
+ * torch_optional_value_copy:
+ * @value: (transfer none): The #TorchOptionalValue to copy.
+ *
+ * Returns: (transfer full): A new #TorchOptionalValue which is a copy of @value
+ */
+TorchOptionalValue *
+torch_optional_value_copy (TorchOptionalValue *value)
+{
+    TorchOptionalValue *copy = g_new0 (TorchOptionalValue, 1);
+    g_value_init (&copy->internal_value, G_VALUE_TYPE (&value->internal_value));
+    g_value_copy (&value->internal_value, &copy->internal_value);
+
+    return copy;
+}
+
+/**
+ * torch_optional_value_free:
+ * @value: The #TorchOptionalValue to free.
+ */
+void
+torch_optional_value_free (TorchOptionalValue *value)
+{
+    g_value_unset (&value->internal_value);
+    g_free (value);
+}
+
+G_DEFINE_BOXED_TYPE (TorchOptionalValue, torch_optional_value, (GBoxedCopyFunc) torch_optional_value_copy, (GBoxedFreeFunc) torch_optional_value_free)
---
+++ torch-gobject/torch-optional-value.h
@@ -0,0 +1,45 @@
+/*
+ * torch-gobject/torch-optional-value.h
+ *
+ * Utility boxed type to represent an optional value. A value of %NULL
+ * means that the value is empty.
+ *
+ * Copyright (C) 2022 Sam Spilsbury.
+ *
+ * torch-gobject is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * torch-gobject is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with torch-gobject; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+typedef struct _TorchOptionalValue TorchOptionalValue;
+
+
+TorchOptionalValue * torch_optional_value_new_double (double value);
+
+TorchOptionalValue * torch_optional_value_copy (TorchOptionalValue *value);
+
+double torch_optional_value_get_double (TorchOptionalValue *value);
+
+void torch_optional_value_free (TorchOptionalValue *value);
+
+GType torch_optional_value_get_type (void);
+#define TORCH_TYPE_OPTIONAL_VALUE (torch_optional_value_get_type ())
+
+
+G_END_DECLS
